7984e912f49c859cb8ebc9237e86af3afbf7a968
Bhekani Khumalo <bhekanik@gmail.com>
2026-03-21 02:10:03 +0000

    fix: render complete homebrew formulas
    Replace the single-URL Homebrew bump with rendered formulas from release checksums.
    Limit the template to supported targets so Intel macOS and Linux get real checksums too.

--- packaging/homebrew/mxr.rb
+++ packaging/homebrew/mxr.rb
@@ -5,20 +5,19 @@ class Mxr < Formula
   license "MIT OR Apache-2.0"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/planetaryescape/mxr/releases/download/v#{version}/mxr-v#{version}-macos-aarch64.tar.gz"
       sha256 "__SHA256_MACOS_AARCH64__"
-    else
+    end
+
+    on_intel do
       url "https://github.com/planetaryescape/mxr/releases/download/v#{version}/mxr-v#{version}-macos-x86_64.tar.gz"
       sha256 "__SHA256_MACOS_X86_64__"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/planetaryescape/mxr/releases/download/v#{version}/mxr-v#{version}-linux-aarch64.tar.gz"
-      sha256 "__SHA256_LINUX_AARCH64__"
-    else
+    on_intel do
       url "https://github.com/planetaryescape/mxr/releases/download/v#{version}/mxr-v#{version}-linux-x86_64.tar.gz"
       sha256 "__SHA256_LINUX_X86_64__"
     end
